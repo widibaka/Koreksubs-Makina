b97be0ac3aa42a719b75f08c1cb86c842a33b529
widibaka <widibaka55@gmail.com>
2020-07-07 14:57:24 +0700

    minor bug fixed

--- application/views/tools/profile_publicly.php
+++ application/views/tools/profile_publicly.php
@@ -169,15 +169,3 @@
           </div>
           <!-- /.col -->
         </div>
-<script type="text/javascript">
-  $(document).ready(function(){
-$('#konfirmasi_hapus_akun').hide()
-      $('#hapus_akun').on('click',function(){
-        $('#konfirmasi_hapus_akun').show('slow')
-      })
-      $('#tidak_jadi_hapus').on('click',function(){
-        $('#konfirmasi_hapus_akun').hide('slow')
-      })
-
-  })
-</script>
